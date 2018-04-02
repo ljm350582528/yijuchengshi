404edccb15d78ee40870e489a508c3e3d1b9b67f
ljm350582528 <350582528@qq.com>
2018-04-02 23:35:09 +0800

    yiju

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+//btn
+{
+    let n=0;
+    $(".al_rjt").click(function () {
+        n++;
+        if(n>1){
+            $("al_rjt").addClass(".active2");
+        }
+        $("al-img").cssText(
+
+        )
+    })
+}
+
+
+//anli
+//
